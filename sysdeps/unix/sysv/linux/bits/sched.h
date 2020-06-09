bbc7c90013ccf0da9e3869fb923cbaa233baeae4
Ali Raza <aliraza@bu.edu>
2020-06-09 16:33:39 -0400

    adding ukl clone flag

--- sysdeps/unix/sysv/linux/bits/sched.h
+++ sysdeps/unix/sysv/linux/bits/sched.h
@@ -71,6 +71,7 @@
 # define CLONE_NEWPID	0x20000000	/* New pid namespace.  */
 # define CLONE_NEWNET	0x40000000	/* New network namespace.  */
 # define CLONE_IO	0x80000000	/* Clone I/O context.  */
+# define CLONE_UKL	0x100000000	/* Unikernel Linux Specific flag */
 #endif
 
 #include <bits/types/struct_sched_param.h>
