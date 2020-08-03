a92d33b1ac0e33315334975fff33421796fa9c14
Ali Raza <aliraza@bu.edu>
2020-08-03 19:57:05 -0400

    mergin changes from ukl

--- sysdeps/x86_64/nptl/tls.h
+++ sysdeps/x86_64/nptl/tls.h
@@ -161,12 +161,12 @@ _Static_assert (offsetof (tcbhead_t, __glibc_unused2) == 0x80,
      _head->self = _thrdescr;						      \
 									      \
      /* It is a simple syscall to set the %fs value for the thread.  */	      \
-     asm volatile ("syscall"						      \
+     asm volatile ("call entry_SYSCALL_64"						      \
 		   : "=a" (_result)					      \
 		   : "0" ((unsigned long int) __NR_arch_prctl),		      \
 		     "D" ((unsigned long int) ARCH_SET_FS),		      \
 		     "S" (_thrdescr)					      \
-		   : "memory", "cc", "r11", "cx");			      \
+		   : "memory", "cc", "r11", "r10");			      \
 									      \
     _result ? "cannot set %fs base address for thread-local storage" : 0;     \
   })
