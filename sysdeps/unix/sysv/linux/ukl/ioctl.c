4f0388a8be8feeb4ccbcb61d3027f75cfcc47516
Isaiah Stapleton <istaplet@redhat.com>
2022-06-30 17:25:37 -0400

    Fix ioctl error

--- sysdeps/unix/sysv/linux/ukl/ioctl.c
+++ sysdeps/unix/sysv/linux/ukl/ioctl.c
@@ -15,17 +15,51 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
-#include <errno.h>
+#include <stdarg.h>
+#include <termios.h>
+#include <unistd.h>
 #include <sys/ioctl.h>
-#include <stdio.h>
+#include <sysdep.h>
 
-/* Perform the I/O control operation specified by REQUEST on FD.
-   The actual type and use of ARG and the return value depend on REQUEST.  */
+/* The user-visible size of struct termios has changed.  Catch ioctl calls
+   using the new-style struct termios, and translate them to old-style.  */
+
 int
 __ioctl (int fd, unsigned long int request, ...)
 {
-	printf("Error: IOCTL should be made as a direct syscall\n");
-	while(1);
-	return -1;
+  void *arg;
+  va_list ap;
+  int result;
+
+  va_start (ap, request);
+  arg = va_arg (ap, void *);
+
+  switch (request)
+    {
+    case TCGETS:
+      result = __tcgetattr (fd, (struct termios *) arg);
+      break;
+
+    case TCSETS:
+      result = __tcsetattr (fd, TCSANOW, (struct termios *) arg);
+      break;
+
+    case TCSETSW:
+      result = __tcsetattr (fd, TCSADRAIN, (struct termios *) arg);
+      break;
+
+    case TCSETSF:
+      result = __tcsetattr (fd, TCSAFLUSH, (struct termios *) arg);
+      break;
+
+    default:
+      result = INLINE_SYSCALL (ioctl, 3, fd, request, arg);
+      break;
+    }
+
+  va_end (ap);
+
+  return result;
 }
-strong_alias (__ioctl, ioctl)
+libc_hidden_def (__ioctl)
+weak_alias (__ioctl, ioctl)
