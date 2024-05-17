3f4dd068130c9a0d0468024a1fbaf2b8d136a764
vance-raiti <vraiti77@gmail.com>
2024-05-17 22:09:36 +0000

    initial commit

--- sysdeps/unix/sysv/linux/x86_64/sigaction.c
+++ sysdeps/unix/sysv/linux/x86_64/sigaction.c
@@ -78,7 +78,8 @@ asm									\
    "	.type __" #name ",@function\n"					\
    "__" #name ":\n"							\
    "	movq $" #syscall ", %rax\n"					\
-   "	syscall\n"							\
+   "	movq entry_SYSCALL_64(%rip), %rcx\n"				\
+   "	call *%rcx\n"							\
    ".LEND_" #name ":\n"							\
    ".section .eh_frame,\"a\",@progbits\n"				\
    ".LSTARTFRAME_" #name ":\n"						\
